c6db66550ea276223be4ff67e4d00348eca29680
Aamir825 <sk0939062@gmail.com>
2024-12-22 18:16:17 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <>
+      <Button>Download Now</Button>
+    </>
+  )
+}
+
+export default App
